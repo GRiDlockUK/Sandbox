1e24f399e56197a7b4264cfabf59ed2fe9268be4
Graham Dimmock <github@grid1.co.uk>
2018-02-25 09:52:57 +0000

    20180225
    various

---
+++ LoadMatrixTest/LoadMatrixTest/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoadMatrixTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string homepath = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            homepath = Environment.GetEnvironmentVariable("homepath");
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLoad_Click(object sender, RoutedEventArgs e)
+        {
+
+            DataTable dt = new DataTable();
+
+            // group list example
+            string[] _groupList = new string[] { "g1", "g2", "g3" };
+            foreach (string _groupName in _groupList)
+            {
+                dt.Columns.Add(_groupName, typeof(string));
+            }
+
+            // user list example
+            string[] _userList = new string[] { "u1", "u2", "u3" };
+            foreach (string _userName in _userList)
+            {
+                DataRow row = dt.NewRow();
+                row[0] = _userName;
+                row[1] = _userName;
+                row[2] = _userName;
+                dt.Rows.Add(row);
+            }
+
+
+            //load data
+            string[] lines = System.IO.File.ReadAllLines(homepath + @"\OneDrive\GitHub\Sandbox\LoadMatrixTest\exampledata2.txt");
+
+            // example with dictionary
+            var dict = new Dictionary<string, string>();
+            foreach (string line in lines)
+            {
+                string[] sections = line.Split(';');
+                dict.Add(sections[0],sections[1]);
+            }
+
+            foreach (KeyValuePair<string, string> entry in dict)
+            {
+                List<string> users = entry.Value.Split(',').ToList<string>();
+                foreach (string user in users)
+                {
+                    DataRow dataRow = dt.NewRow();
+                    dataRow[0] = "user";
+                    dataRow[1] = user;
+                    dt.Rows.Add(dataRow);
+
+                }
+            }
+
+            dataGrid.ItemsSource = dt.DefaultView;
+        }
+
+     public class _groupUserData
+     {
+         public string group { get; set; }
+         public List<string> user { get; set; }
+
+         public _groupUserData(string gp, List<string> us)
+         {
+             this.group = gp;
+             this.user = us;
+         }
+
+         public string returnCSV()
+         {
+             //return "\"" + this.user + "\",\"" + this.group + "\"";
+             return null;
+         }
+
+
+     }
+    }
+    
+
+
+
+
+
+}
---
+++ Pointless/Pointless/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Pointless
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.tbMain = new System.Windows.Forms.TextBox();
+            this.lblEnv = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(197, 226);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // tbMain
+            // 
+            this.tbMain.Location = new System.Drawing.Point(16, 12);
+            this.tbMain.Multiline = true;
+            this.tbMain.Name = "tbMain";
+            this.tbMain.Size = new System.Drawing.Size(256, 208);
+            this.tbMain.TabIndex = 1;
+            // 
+            // lblEnv
+            // 
+            this.lblEnv.AutoSize = true;
+            this.lblEnv.Location = new System.Drawing.Point(13, 226);
+            this.lblEnv.Name = "lblEnv";
+            this.lblEnv.Size = new System.Drawing.Size(29, 13);
+            this.lblEnv.TabIndex = 2;
+            this.lblEnv.Text = "DEV";
+            this.lblEnv.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblEnv);
+            this.Controls.Add(this.tbMain);
+            this.Controls.Add(this.btnExit);
+            this.Name = "frmMain";
+            this.Text = "Pointless";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox tbMain;
+        private System.Windows.Forms.Label lblEnv;
+    }
+}
+
